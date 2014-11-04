8852cd6cae91d3320f9596f28fa3d81dd9c0b7d6
Jamie Hale <jamie@smallarmyofnerds.com>
2014-11-04 08:50:45 -0500

    Convert specs to expect a tuple response

--- ruby-add.rb
+++ ruby-add.rb
@@ -7,11 +7,11 @@ end
 describe 'array element adder' do
   
   it 'should return 0.0 for an empty input' do
-    expect( sum_elements( [] ) ).to eq( 0.0 )
+    expect( sum_elements( [] ) ).to eq( [ 0.0, 0.0 ] )
   end
   
   it 'should return the first element for a single element array' do
-    expect( sum_elements( [ 3.1415926 ] ) ).to eq( 3.1415926 )
+    expect( sum_elements( [ 3.1415926 ] ) ).to eq( [ 3.1415926, 0.0 ] )
   end
   
 end
