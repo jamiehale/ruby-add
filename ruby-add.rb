b36275ee7411ded973b7f93247a01862636c2ec3
Jamie Hale <jamie@smallarmyofnerds.com>
2014-11-04 08:40:26 -0500

    Convert to useful first spec

--- ruby-add.rb
+++ ruby-add.rb
@@ -1,8 +1,8 @@
 
 describe 'array element adder' do
   
-  it 'should fail' do
-    fail
+  it 'should return 0.0 for an empty input' do
+    expect( sum_elements( [] ) ).to eq( 0.0 )
   end
   
 end
